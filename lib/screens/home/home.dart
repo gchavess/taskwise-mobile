0877d8bbf4119184912cff115841727d708d1b62
Guilherme Melato <gmelato28@gmail.com>
2023-11-09 09:46:55 -0300

    HomePage

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -51,7 +51,6 @@ class HomePage extends StatelessWidget {
                             padding: const EdgeInsets.all(10),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
                                   'Atividade',
@@ -71,83 +70,151 @@ class HomePage extends StatelessWidget {
               Container(
                   height: MediaQuery.of(context).size.height,
                   width: double.infinity,
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                    left: 15,
-                  ),
+                  padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
                   decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(50),
                           topRight: Radius.circular(50))),
-                  // child: Column(
-                  //   crossAxisAlignment: CrossAxisAlignment.start,
-                  //   children: [
-                  //     Container(
-                  //       padding: const EdgeInsets.all(8),
-                  //       decoration: const BoxDecoration(
-                  //           shape: BoxShape.circle, color: Colors.blue),
-                  //       child: const Icon(
-                  //         Icons.add,
-                  //         color: Color.fromARGB(255, 255, 88, 88),
-                  //         size: 40,
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.all(9),
-                          constraints: const BoxConstraints(
-                              minHeight: 0, maxHeight: 120),
-                          decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 255, 255, 255),
-                              borderRadius: BorderRadius.circular(9),
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Color.fromARGB(77, 0, 0, 0),
-                                  blurRadius: 6,
-                                  spreadRadius: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20, left: 10, right: 10, bottom: 100),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(9),
+                            constraints: const BoxConstraints(
+                                minHeight: 0, maxHeight: 95),
+                            decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 255, 255, 255),
+                                borderRadius: BorderRadius.circular(9),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromARGB(44, 0, 0, 0),
+                                    blurRadius: 6,
+                                    spreadRadius: 4,
+                                  ),
+                                ]),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(2),
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    shape: BoxShape.rectangle,
+                                  ),
+                                  child: const Icon(
+                                    Icons.calendar_today_rounded,
+                                    color: Color.fromARGB(255, 0, 71, 178),
+                                    size: 50,
+                                  ),
                                 ),
-                              ]),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(2),
-                                decoration: const BoxDecoration(
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  shape: BoxShape.rectangle,
-                                ),
-                                child: const Icon(
-                                  Icons.calendar_today_rounded,
-                                  color: Color.fromARGB(255, 0, 71, 178),
-                                  size: 75,
-                                ),
-                              ),
-                              const SizedBox(height: 1),
-                              Text(
-                                'Calendário',
-                                style: GoogleFonts.poppins(
-                                    color:
-                                        const Color.fromARGB(255, 45, 45, 45)),
-                              )
-                            ],
+                                Text(
+                                  'Calendário',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 13,
+                                      color: const Color.fromARGB(
+                                          255, 45, 45, 45)),
+                                )
+                              ],
+                            ),
                           ),
-                          // child: const Text(
-                          //   'Opaaaa',
-                          //   style: TextStyle(
-
-                          //   ),
-                          // ),
                         ),
-                      )
-                    ],
+                        const SizedBox(width: 18),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(9),
+                            constraints: const BoxConstraints(
+                                minHeight: 0, maxHeight: 95),
+                            decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 255, 255, 255),
+                                borderRadius: BorderRadius.circular(9),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromARGB(44, 0, 0, 0),
+                                    blurRadius: 6,
+                                    spreadRadius: 4,
+                                  ),
+                                ]),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(2),
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    shape: BoxShape.rectangle,
+                                  ),
+                                  child: const Icon(
+                                    Icons.watch_later_outlined,
+                                    color: Color.fromARGB(255, 0, 71, 178),
+                                    size: 50,
+                                  ),
+                                ),
+                                Text(
+                                  'Pomodoro',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 15,
+                                      color: const Color.fromARGB(
+                                          255, 45, 45, 45)),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 18),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(9),
+                            width: 100,
+                            constraints: const BoxConstraints(
+                                minHeight: 0, maxHeight: 95),
+                            decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 255, 255, 255),
+                                borderRadius: BorderRadius.circular(9),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromARGB(44, 0, 0, 0),
+                                    blurRadius: 6,
+                                    spreadRadius: 4,
+                                  ),
+                                ]),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(2),
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    shape: BoxShape.rectangle,
+                                  ),
+                                  child: const Icon(
+                                    Icons.layers_outlined,
+                                    color: Color.fromARGB(255, 0, 71, 178),
+                                    size: 50,
+                                  ),
+                                ),
+                                Text(
+                                  'Metas',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 15,
+                                      color: const Color.fromARGB(
+                                          255, 45, 45, 45)),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   )),
+              const SizedBox(height: 15)
             ],
           ),
         ));
