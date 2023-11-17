c210e9d41c4bfc8c159be45d00966bb12537e80f
Guilherme Melato <gmelato28@gmail.com>
2023-11-16 22:38:32 -0300

    Grid goals

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -45,7 +45,7 @@ class HomePage extends StatelessWidget {
 
   Widget buildGreetingText() {
     return Text(
-      'Olá, ${userName}!',
+      'Olá, $userName!',
       style: GoogleFonts.poppins(
         fontSize: 20,
         color: Colors.white,
@@ -57,13 +57,10 @@ class HomePage extends StatelessWidget {
       String label, IconData icon, Color iconColor, VoidCallback onPressed) {
     return Expanded(
       child: InkWell(
-        onTap: () {
-          // Navegar para a rota correspondente quando o botão é pressionado
-          onPressed;
-        },
+        onTap: onPressed,
         child: Container(
           padding: const EdgeInsets.all(7),
-          constraints: const BoxConstraints(minHeight: 0, maxHeight: 95),
+          constraints: const BoxConstraints(minHeight: 0),
           decoration: BoxDecoration(
             color: const Color.fromARGB(255, 255, 255, 255),
             borderRadius: BorderRadius.circular(9),
@@ -104,6 +101,86 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Widget buildButtonsRow() {
+    return SizedBox(
+      height: 100, // Defina a altura desejada para a Row
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildButton('Calendário', Icons.calendar_today_rounded,
+              const Color.fromARGB(255, 0, 71, 178), () {}),
+          const SizedBox(width: 18),
+          buildButton('Pomodoro', Icons.watch_later_outlined,
+              const Color.fromARGB(255, 0, 71, 178), () {}),
+          const SizedBox(width: 18),
+          buildButton('Metas', Icons.layers_outlined,
+              const Color.fromARGB(255, 0, 71, 178), () => const MyGoals()),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWhiteRectangle() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.only(top: 25, left: 25, right: 22),
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildInkWell(),
+              buildInkWell(),
+              buildInkWell(),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildInkWell(),
+              buildInkWell(),
+              buildInkWell(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildInkWell() {
+    return InkWell(
+      onTap: () {
+        // Lidar com o toque no InkWell
+      },
+      child: Container(
+        width: 95,
+        height: 95,
+        color: Colors.blue,
+        margin: const EdgeInsets.all(5),
+        child: const Center(
+          child: Text(
+            'Item',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,9 +201,9 @@ class HomePage extends StatelessWidget {
             ),
             const SizedBox(height: 100),
             Container(
-              height: MediaQuery.of(context).size.height,
               width: double.infinity,
-              padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+              padding: const EdgeInsets.only(
+                  top: 30, left: 20, right: 20, bottom: 20),
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -134,26 +211,19 @@ class HomePage extends StatelessWidget {
                   topRight: Radius.circular(35),
                 ),
               ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildButton('Calendário', Icons.calendar_today_rounded,
-                      const Color.fromARGB(255, 0, 71, 178), () {}),
-                  const SizedBox(width: 18),
-                  buildButton('Pomodoro', Icons.watch_later_outlined,
-                      const Color.fromARGB(255, 0, 71, 178), () {}),
-                  const SizedBox(width: 18),
-                  buildButton('Metas', Icons.layers_outlined,
-                      const Color.fromARGB(255, 0, 71, 178), () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MyGoals()));
-                  }),
-                ],
-              ),
+              child: buildButtonsRow(),
             ),
+            Container(
+              color: Colors.white, // Cor do fundo branco
+              width: double.infinity,
+              height: 275,
+              child: buildWhiteRectangle(),
+            ),
+            Container(
+              color: Colors.white, // Cor do fundo branco
+              width: double.infinity,
+              height: 275,
+            )
           ],
         ),
       ),
